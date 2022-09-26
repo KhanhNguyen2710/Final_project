29af39e657d0a40adcca4c69f7f540f9a2cfff8c
Khánh Nguyễn <hanaaimi304@gmail.com>
2022-09-26 17:15:57 +0700

    Project

---
+++ app/src/main/java/com/example/myapplication/Database.java
@@ -0,0 +1,42 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper {
+// change name
+    private Context context;
+    private static final String DATABASE_NAME = "TripLibrary.db";
+    private static final int DATABASE_VERSION = 1 ;
+    // create table data
+    private static final String TABLE_NAME = "trip_library" ;
+    private static final String COLUMN_ID = "_id" ;
+    private static final String COLUMN_TITLE = "product_title" ;
+    private static final String COLUMN_SELLER = "product_seller" ;
+    private static final String COLUMN_QUALITY = "product_quality" ;
+
+    // Database == MyDatabaseHelper
+    public Database(@Nullable Context context) {
+        super(context, DATABASE_NAME, null , DATABASE_VERSION);
+        this.context = context;
+    }
+// change name
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLE_NAME +
+                        " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_TITLE + " TEXT, " +
+                        COLUMN_SELLER + " TEXT, " +
+                        COLUMN_QUALITY + " INTEGER);";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+}
